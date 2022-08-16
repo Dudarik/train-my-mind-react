051777ea5a7746ea1eaab9efb5bb07485c4cb754
Dudarik <dudarik@yandex.ru>
2022-08-16 21:31:39 +0500

    maby need use redux/tolkit? or im idiot

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,12 +1,12 @@
-import { createContext, ReactNode } from "react";
+import React, { createContext, ReactNode } from "react";
 import { IGameContext } from "../interfaces/IGameContext";
 
 export const GameContext = createContext<IGameContext | null>(null);
 
-export const contextProvider = ({ children }) => {
+export const contextProvider = (children: React.FC) => {
   const contextValue: IGameContext = {
     cards: [{ id: 1, cardType: "lamp", count: 2, color: "red" }],
     score: 0,
   };
-  return <GameContext.provider>{children}</GameContext.provider>;
+  // return (<GameContext.provider value={contextValue}>{children}</GameContext.provider>)
 };
