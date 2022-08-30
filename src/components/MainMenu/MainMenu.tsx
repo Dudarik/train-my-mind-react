1ab72a63c45b36febc01bd44f82dc58922e01785
Alex <Dudarik@yandex.ru>
2022-08-30 15:05:51 +0500

    add toggle menu button

--- src/components/MainMenu/MainMenu.tsx
+++ src/components/MainMenu/MainMenu.tsx
@@ -1,19 +1,23 @@
 import { NavLink } from "react-router-dom";
 import Logo from "../Logo/Logo";
 import "./MainMenu.scss";
+import ToggleMenu from "./ToggleMenu";
 
 const MainMenu: React.FC = () => {
   return (
     <div className='mainmenu'>
-      <div className='logo'>
-        <Logo />
-        <div className='title'>
-          <h2>TRAIN</h2>
-          <h2>MY</h2>
-          <h2>MIND</h2>
+      <NavLink className='nav-link' to='/'>
+        <div className='logo'>
+          <Logo />
+          <div className='title'>
+            <h2>TRAIN</h2>
+            <h2>MY</h2>
+            <h2>MIND</h2>
+          </div>
         </div>
-      </div>
+      </NavLink>
       <div className='menu'>
+        <ToggleMenu />
         <menu>
           <NavLink
             className={({ isActive }) =>
---
+++ src/components/MainMenu/ToggleMenu.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+const ToggleMenu = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleOnClick = () => {
+    setIsActive((prev) => !prev);
+  };
+
+  return (
+    <div
+      className={isActive ? "toggle_menu toggle_menu__active" : "toggle_menu"}
+      onClick={handleOnClick}>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+};
+
+export default ToggleMenu;
